5af08ae82e083195fd714971cdc2ff0920396a5b
Daniel Santos <danny.pinkney@gmail.com>
2025-03-19 21:35:49 +0000

    Btter sync and benching

--- koka/unix/output-bench.sh
+++ koka/unix/output-bench.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 setup_sync() {
-    koka benches/sync_param.kk --stack 16000000 -o benches/out/sync_param.kk.out -O=0
-    koka benches/sync_ref.kk   --stack 16000000 -o benches/out/sync_ref.kk.out   -O=0
-    koka benches/sync_state.kk --stack 16000000 -o benches/out/sync_state.kk.out -O=0
+    # Stack size
+    $stack_size = 0 #300000000
+
+    koka benches/sync_param.kk --stack $stack_size -o benches/out/sync_param.kk.out
+    koka benches/sync_ref.kk   --stack $stack_size -o benches/out/sync_ref.kk.out
+    koka benches/sync_state.kk --stack $stack_size -o benches/out/sync_state.kk.out
 
     chmod u+x benches/out/sync_param.kk.out
     chmod u+x benches/out/sync_ref.kk.out
@@ -13,7 +16,7 @@ setup_sync() {
 run_sync() {
     hyperfine \
         --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'  \
-        --parameter-scan n $1 $2 -D 1 \
-        --runs 5 './benches/out/sync_ref.kk.out {n}' './benches/out/sync_param.kk.out {n}' './benches/out/sync_state.kk.out {n}'
+        --parameter-scan n ${2:-15} ${3:-$2} -D 1 \
+        --runs ${1:-5} './benches/out/sync_ref.kk.out {n}' './benches/out/sync_param.kk.out {n}' './benches/out/sync_state.kk.out {n}'
 }
 
