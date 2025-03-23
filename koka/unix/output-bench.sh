5623891d82673109e080da0025e2c970c278b319
Daniel Santos <danny.pinkney@gmail.com>
2025-03-23 12:56:04 +0000

    Most Benchmarks

--- koka/unix/output-bench.sh
+++ koka/unix/output-bench.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 setup_sync() {
-    # Stack size
-    $stack_size = 0 #300000000
+    STACK_SIZE = 0 #300000000
 
-    koka benches/sync_param.kk --stack $stack_size -o benches/out/sync_param.kk.out
-    koka benches/sync_ref.kk   --stack $stack_size -o benches/out/sync_ref.kk.out
-    koka benches/sync_state.kk --stack $stack_size -o benches/out/sync_state.kk.out
+    koka -c benches/sync_param.kk --stack "$STACK_SIZE" -o benches/out/sync_param.kk.out
+    koka -c benches/sync_ref.kk   --stack "$STACK_SIZE" -o benches/out/sync_ref.kk.out
+    koka -c benches/sync_state.kk --stack "$STACK_SIZE" -o benches/out/sync_state.kk.out
 
     chmod u+x benches/out/sync_param.kk.out
     chmod u+x benches/out/sync_ref.kk.out
@@ -20,3 +19,37 @@ run_sync() {
         --runs ${1:-5} './benches/out/sync_ref.kk.out {n}' './benches/out/sync_param.kk.out {n}' './benches/out/sync_state.kk.out {n}'
 }
 
+
+setup_fs() {
+    koka benches/fs_fs.kk -o benches/out/fs_fs.kk.out
+    koka benches/fs_unified.kk -o benches/out/fs_unified.kk.out
+
+    chmod u+x benches/out/fs_fs.kk.out
+    chmod u+x benches/out/fs_unified.kk.out
+}
+
+run_fs() {
+    hyperfine \
+        --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'  \
+        --parameter-scan n ${2:-1000} ${3:-$2} -D 100 \
+        --runs ${1:-5} './benches/out/fs_unified.kk.out {n}' './benches/out/fs_fs.kk.out {n}'
+}
+
+setup_session() {
+    koka benches/session_1.kk -o benches/out/session_1.kk.out
+    koka benches/session_2.kk -o benches/out/session_2.kk.out
+    koka benches/session_3.kk -o benches/out/session_3.kk.out
+
+    chmod u+x benches/out/session_1.kk.out
+    chmod u+x benches/out/session_2.kk.out
+    chmod u+x benches/out/session_3.kk.out
+}
+
+run_session() {
+hyperfine \
+    --prepare 'sync; echo 3 | sudo tee /proc/sys/vm/drop_caches'  \
+    --parameter-scan n ${2:-1000} ${3:-$2} -D 100 \
+    --runs ${1:-5} './benches/out/session_1.kk.out {n}' './benches/out/session_2.kk.out {n}' './benches/out/session_3.kk.out {n}' \
+    --export-csv benches/out/session.csv
+
+}
